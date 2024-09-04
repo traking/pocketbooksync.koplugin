28765414ec7bb8ee111074a282346af3a0dc4b5f
traking <mrdanvern@gmail.com>
2024-09-04 20:15:22 +0700

    Use libbookstate.so

--- pocketbooksync.koplugin/main.lua
+++ pocketbooksync.koplugin/main.lua
@@ -9,27 +9,18 @@ local _ = require("gettext")
 local logger = require("logger")
 local util = require("util")
 local UIManager = require("ui/uimanager")
-local SQ3 = require("lua-ljsqlite3/init")
-local pocketbookDbConn = SQ3.open("/mnt/ext1/system/explorer-3/explorer-3.db")
+
 local ffi = require("ffi")
-local inkview = ffi.load("inkview")
 
--- wait for database locks for up to 1 second before raising an error
-pocketbookDbConn:set_busy_timeout(1000)
-
-local function GetCurrentProfileId()
-    local profile_name = inkview.GetCurrentProfile()
-    if profile_name == nil then
-        return 1
-    else
-        local stmt = pocketbookDbConn:prepare("SELECT id FROM profiles WHERE name = ?")
-        local profile_id = stmt:reset():bind(ffi.string(profile_name)):step()
-        stmt:close()
-        return profile_id[1]
-    end
-end
-
-local profile_id = GetCurrentProfileId()
+ffi.cdef[[
+void *bsLoad(const char *);
+void bsSetCPage(void *, int);
+void bsSetNPage(void *, int);
+void bsSetOpenTime(void *bstate, time_t);
+int bsClose(void *);
+int bsSave(void *);
+]]
+local pb_bookstate = ffi.load("/usr/lib/libbookstate.so")
 
 local PocketbookSync = WidgetContainer:extend{
     name = "pocketbooksync",
@@ -54,8 +45,8 @@ function PocketbookSync:prepareSync()
         return nil
     end
 
-    local folder, file = self:getFolderFile()
-    if not folder or folder == "" or not file or file == "" then
+    local filepath = self.view.document.file
+    if not filepath or filepath == "" then
         logger.info("Pocketbook Sync: No folder/file found for " .. self.view.document.file)
         return nil
     end
@@ -71,10 +62,6 @@ function PocketbookSync:prepareSync()
     local totalPages = self.document:getTotalPagesInFlow(flow)
     local page = self.document:getPageNumberInFlow(globalPage)
 
-    local summary = self.ui.doc_settings:readSetting("summary")
-    local status = summary and summary.status
-    local completed = (status == "complete" or page == totalPages) and 1 or 0
-
     -- hide the progress bar if we're on the title/cover page
     --
     -- we'll never set cpage=1 so the progress bar will seem to jump a bit at
@@ -87,11 +74,9 @@ function PocketbookSync:prepareSync()
     end
 
     local data = {
-        folder = folder,
-        file = file,
+        filepath = filepath,
         totalPages = totalPages,
         page = page,
-        completed = completed,
         time = os.time(),
     }
     return data
@@ -101,43 +86,16 @@ function PocketbookSync:doSync(data)
     if not data then
         return
     end
-
-    local sql = [[
-            SELECT book_id
-            FROM files
-            WHERE
-                folder_id = (SELECT id FROM folders WHERE name = ? LIMIT 1)
-            AND filename = ?
-            LIMIT 1
-        ]]
-    local stmt = pocketbookDbConn:prepare(sql)
-    local row = stmt:reset():bind(data.folder, data.file):step()
-    stmt:close()
-
-    if row == nil then
-        logger.info("Pocketbook Sync: Book id for " .. data.folder .. "/" .. data.file .. " not found")
+    local handle = pb_bookstate.bsLoad(data.filepath)
+    if not handle then
         return
     end
-    local book_id = row[1]
 
-    local sql = [[
-            REPLACE INTO books_settings
-            (bookid, profileid, cpage, npage, completed, opentime)
-            VALUES (?, ?, ?, ?, ?, ?)
-        ]]
-    local stmt = pocketbookDbConn:prepare(sql)
-    stmt:reset():bind(book_id, profile_id, data.page, data.totalPages, data.completed, data.time):step()
-    stmt:close()
-end
-
-function PocketbookSync:getFolderFile()
-    local path = self.view.document.file
-    local folder, file = util.splitFilePathName(path)
-    local folderTrimmed = folder:match("(.*)/")
-    if folderTrimmed ~= nil then
-        folder = folderTrimmed
-    end
-    return folder, file
+    pb_bookstate.bsSetCPage(handle, data.page)
+    pb_bookstate.bsSetNPage(handle, data.totalPages)
+    pb_bookstate.bsSetOpenTime(handle, data.time)
+    pb_bookstate.bsSave(handle)
+    pb_bookstate.bsClose(handle)
 end
 
 function PocketbookSync:onPageUpdate()
